84b7564baddd73e17c21b35a839b9782edb98d5b
mgomoka <86494961+mgomoka@users.noreply.github.com>
2021-07-07 00:00:57 -0400

    commit

--- VSCode-Python/uMichPhysics/rydberg.py
+++ VSCode-Python/uMichPhysics/rydberg.py
@@ -9,7 +9,7 @@ for m in range(1, 4):
 
 
 
-howmanyMdoyouwant = 12
+howmanyMdoyouwant = 50
 
 R = 1.097e-2
 m = [[] for _ in range(howmanyMdoyouwant)]
@@ -50,3 +50,43 @@ print("M", m[2][0], " = ", m[2][1], m[2][2], m[2][3], m[2][4], m[2][5])
 
 import pygame as pg
 
+pg.init()
+pg.display.set_caption("Rydberg")
+
+size = (850, 850)
+screen = pg.display.set_mode(size)
+clock = pg.time.Clock()
+font = pg.font.SysFont('avenir', 12)
+
+while True:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+
+    screen.fill((255, 255, 255))
+
+    oneString = 'The emission spectrum of atomic hydrogen has been divided into a number of spectral series, with wavelengths given by the Rydberg formula.'
+    text = font.render(oneString, True, (0, 0, 0))
+    screen.blit(text, [20, 20])
+    twoString = 'These observed spectral lines are due to the electron making transitions between two energy levels in an atom.'
+    text = font.render(twoString, True, (0, 0, 0))
+    screen.blit(text, [20, 32])
+    threeString = 'The classification of the series by the Rydberg formula was important in the development of quantum mechanics.'
+    text = font.render(threeString, True, (0, 0, 0))
+    screen.blit(text, [20, 44])
+    fourString = 'The spectral series are important in astronomical spectroscopy for detecting the presence of hydrogen and calculating red shifts.'
+    text = font.render(fourString, True, (0, 0, 0))
+    screen.blit(text, [20, 56])
+
+    varListTwoPointO = []
+    for x in range(1, howmanyMdoyouwant+1):
+        for y in range(0, 5):
+            varListTwoPointO.append(m[x-1][y+1])
+        if x <= 50:
+            textTemp = 'M' + str(m[x-1][0]) + " = " + str(varListTwoPointO)
+            textTwo = font.render(textTemp, True, (0, 0, 0))
+            screen.blit(textTwo, [20, 200+(12*x)])
+        varListTwoPointO.clear()
+
+    pg.display.update()
+    clock.tick(60)
