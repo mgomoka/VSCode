2384b430cd9920e96fb3cbc9bdf30e1ec3285ec7
mgomoka <86494961+mgomoka@users.noreply.github.com>
2021-07-08 12:52:51 -0400

    commit

---
+++ VSCode-Python/TSLAstock/StockPricePrediction.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+
+teslaDataSet = pd.read_csv('TSLA.csv')
+
+teslaDataSet = teslaDataSet[['Date', 'Close']]
+newTesla = teslaDataSet.loc[884:1639]
+newTesla = newTesla.drop('Date', axis = 1)
+newTesla = newTesla.reset_index(Drop = True)
+T = T.astype('float32')
+T = np.eshape(T, (-1, 1))
+
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler(feature_range=(0, 1))
+T = scaler.fit_transform(T)
+
