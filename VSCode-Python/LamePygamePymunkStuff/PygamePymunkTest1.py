3c9cb90ae74d5e0221e66d82656a85dda3d18e2e
mgomoka <86494961+mgomoka@users.noreply.github.com>
2021-07-13 13:50:56 -0400

    commit

--- VSCode-Python/LamePygamePymunkStuff/PygamePymunkTest1.py
+++ VSCode-Python/LamePygamePymunkStuff/PygamePymunkTest1.py
@@ -7,3 +7,20 @@ import numpy as np
 
 
 
+# Define Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BWHITE = (245, 245, 220)
+RED = (255, 99, 71)
+BLUE = (30, 144, 255)
+YELLOW = (255, 255, 0)
+
+
+pg.init()
+pg.display.set_caption('PyGamePyMunkTest1')
+
+size = (850, 850)
+screen = pg.display.set_mode(size)
+clock = pg.time.Clock()
+font = pg.font.Font('freesansbold.ttf', 15) # pg.font.SysFont('avenir', 15)
+
