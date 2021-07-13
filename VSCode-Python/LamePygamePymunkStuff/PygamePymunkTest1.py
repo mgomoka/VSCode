986650033970293ccba55a5a49b4ac1183193be3
mgomoka <86494961+mgomoka@users.noreply.github.com>
2021-07-13 18:13:34 -0400

    commit

--- VSCode-Python/LamePygamePymunkStuff/PygamePymunkTest1.py
+++ VSCode-Python/LamePygamePymunkStuff/PygamePymunkTest1.py
@@ -6,14 +6,13 @@ import random
 import numpy as np
 
 
-
-# Define Colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 BWHITE = (245, 245, 220)
 RED = (255, 99, 71)
 BLUE = (30, 144, 255)
 YELLOW = (255, 255, 0)
+
 
 
 pg.init()
