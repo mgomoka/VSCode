e440c08187d6850f1597187eeb4ae50aa8d43f79
Matthew Gomoka <mgomoka@bu.edu>
2021-07-23 19:09:28 -0400

    commit

---
+++ VSCode-Java/CourseraAlgorithmsPart1/WeekOne/PracticeProgrammingAssignment/HelloWorld.java
@@ -0,0 +1,8 @@
+package CourseraAlgorithmsPart1.WeekOne.PracticeProgrammingAssignment;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Prints "Hello, World" To The Terminal Window
+        System.out.println("Hello, World");
+    }
+}
