ba3fb7551a2ecd359980da1aa85ed48c2d3ead35
Matthew Gomoka <mgomoka@bu.edu>
2021-07-25 13:40:26 -0400

    commit

---
+++ VSCode-Python/Cryptography/Cryptography.py
@@ -0,0 +1,5 @@
+from cryptography.fernet import Fernet
+
+file = open('/Users/matthewgomoka/Documents/VSCode/VSCode-Python/Cryptography/Cryptography.txt', 'w')
+file.write("Added Text From Python\n")
+file.close()
