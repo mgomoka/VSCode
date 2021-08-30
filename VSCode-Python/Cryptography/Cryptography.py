18c4ff6758da5174065ab82810b52e918fa984e5
Matthew Gomoka <mgomoka@bu.edu>
2021-08-30 13:00:46 -0400

    commit

--- VSCode-Python/Cryptography/Cryptography.py
+++ VSCode-Python/Cryptography/Cryptography.py
@@ -4,4 +4,6 @@ from cryptography.fernet import Fernet
 def generateMasterKey():
     master_key = Fernet.generate_key()
     with open('/Users/matthewgomoka/Documents/VSCode/VSCode-Python/Cryptography/MasterKey.txt', 'w') as master_key_file:
-        master_key_file.write(master_key)
+        master_key_file.write(str(master_key))
+
+generateMasterKey()
